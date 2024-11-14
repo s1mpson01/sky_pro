176020b4d824ecf9034f4c009915321a59bdf699
Konstantin Borodaev <igorektroickij@gmail.com>
2024-11-14 20:31:08 +0300

    add func mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,17 @@
+import re
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(account_card_number: str) -> str:
+    """Обрабатывает информацию о картах, счетах и возвращает их маску"""
+
+    type_account = re.sub(r"\d","", account_card_number)
+    number_account = re.sub(r"\D", "",account_card_number)
+    match = re.search("Счет", account_card_number)
+
+    if match:
+        mask_account_card_number = type_account + get_mask_account(int(number_account))
+    else:
+        mask_account_card_number = type_account + get_mask_card_number(int(number_account))
+
+    return mask_account_card_number
