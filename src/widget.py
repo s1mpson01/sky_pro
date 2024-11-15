5c165b2ca9f0f3345e472470752497daff1287ba
Konstantin Borodaev <igorektroickij@gmail.com>
2024-11-15 21:22:03 +0300

    use isort, flake8, mypy

--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,19 @@
 import re
-from masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card_number: str) -> str:
     """Обрабатывает информацию о картах, счетах и возвращает их маску"""
 
     type_account = re.sub(r"\d","", account_card_number)
-    number_account = re.sub(r"\D", "",account_card_number)
+    number_account = re.sub(r"\D", "", account_card_number)
     match = re.search("Счет", account_card_number)
 
     if match:
-        mask_account_card_number = type_account + get_mask_account(int(number_account))
+        mask_account_card_number = str(type_account + get_mask_account(int(number_account)))
     else:
-        mask_account_card_number = type_account + get_mask_card_number(int(number_account))
+        mask_account_card_number = str(type_account + get_mask_card_number(int(number_account)))
 
     return mask_account_card_number
 
