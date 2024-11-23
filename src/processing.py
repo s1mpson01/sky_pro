a8fa582feedbeba3079899b5218faea841a14d15
Konstantin Borodaev <igorektroickig@gmail.com>
2024-11-23 22:00:50 +0300

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,18 @@
 def filter_by_state(my_list, parameter="EXECUTED"):
-    """Принимает список словарей и значение для ключа state.
+    """Принимает список словарей и значение для ключа state.(по умолчанию 'EXECUTED')
     Возвращает новый список словарей, у которых ключ state
     соответствует указанному значению."""
 
     return [line for line in my_list if line.get("state") == parameter]
+
+
+def sort_by_date(my_list, sort_parameter=True):
+    """Принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    Возвращает новый список, отсортированный по дате (date)."""
+
+    if sort_parameter:
+        text_sort = sorted(my_list, key=lambda x: x.get("date"), reverse=True)
+    else:
+        text_sort = sorted(my_list, key=lambda x: x.get("date"), reverse=False)
+    return text_sort
