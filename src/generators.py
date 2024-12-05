21f5a53f4e49f3eb1aa682f967020154f83ca540
Konstantin Borodaev <igorektroickig@gmail.com>
2024-12-05 19:17:28 +0300

    add func transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -4,7 +4,16 @@ def filter_by_currency(my_list, currency):
     Возвращать итератор, который поочередно выдает транзакции,
     где валюта операции соответствует заданной.
     """
-    result = (i for i in my_list if i.get("operationAmount").get("currency").get("code")
-              == currency)
 
+    result = (i for i in my_list if i.get("operationAmount").get("currency").get("code") == currency)
+    return result
+
+
+def transaction_descriptions(my_list):
+    """
+    Генератор, который принимает список словарей с транзакциями
+    и возвращает описание каждой операции по очереди.
+    """
+
+    result = (i.get("description") for i in my_list)
     return result
