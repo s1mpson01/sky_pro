e22d3b247987a6136f5d52b1e3dd290410d336df
Konstantin Borodaev <igorektroickig@gmail.com>
2025-01-16 20:54:12 +0300

    add tests for new func

--- src/reading.py
+++ src/reading.py
@@ -1,13 +1,27 @@
 import pandas as pd
 
 
-def get_transactions_from_excel(path):
-    df = pd.read_excel(f"{path}")
-    result = df.to_dict(orient="records")
-    return result
+def get_transactions_from_excel(path: str) -> list:
+    """
+    Cчитывает финансовые операций из excel-файла
+    """
+    try:
+        df = pd.read_excel(f"{path}")
+        result = df.to_dict(orient="records")
+    except Exception:
+        raise Exception("Excel файл не найден")
+    else:
+        return result
 
 
-def get_transactions_from_csv(path):
-    df = pd.read_csv(f"{path}", sep=";")
-    result = df.to_dict(orient="records")
-    return result
+def get_transactions_from_csv(path: str) -> list:
+    """
+    Cчитывает финансовые операций из CSV-файла
+    """
+    try:
+        df = pd.read_csv(f"{path}", sep=";")
+        result = df.to_dict(orient="records")
+    except Exception:
+        raise Exception("Csv файл не найден")
+    else:
+        return result
